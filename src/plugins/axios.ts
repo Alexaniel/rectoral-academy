c1ca80cf68ce1cd982fd7c8d7c1c746438a8da6d
Alex Castillo <alexc@pickerexpress.com>
2022-11-11 11:07:31 -0500

    feat(axios-plugin): Added control access origin

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import i18n from './i18n';
 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['content-language'] = i18n.language || 'es';
 axios.defaults.headers.common.utcoffset = '-300';
